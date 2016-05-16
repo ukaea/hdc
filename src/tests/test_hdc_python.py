b9c3d622c703a15db671a34ed8f56158a59d4873
David Fridrich <d.fridrich@volny.cz>
2016-05-16 16:58:27 +0200

    Added short yet functional example of python API.

--- src/tests/test_hdc_python.py
+++ src/tests/test_hdc_python.py
@@ -1,14 +1,10 @@
-import hdc_python as hdc
-import numpy as np
-c = hdc.Container(np.array([1,2,3],dtype="int64"), 3, 11)
-print(c[0],c[1],c[2])
-c[0] = 111
-print(c)
-print(c[0],c[1],c[2])
+# to be run from the same folder where libhdc_fortran is
+import libhdc_python as hdc
 
-t = hdc.HDC()
-t.addNode("aaa/bbb",c)
-cc = t.getNode("aaa/bbb")
-print("end of get")
-print(cc)
-print(cc[0],cc[1],cc[2])
+hdc.hello__()
+h = hdc.hdc()
+g = hdc.hdc()
+h.add_child("aaa/bbb",g)
+h.set_data_double("aaa/bbb", 55.5)
+k = h.get_child("aaa")
+k.set_data_int32("bbb",323232)
