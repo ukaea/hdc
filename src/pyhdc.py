d36cc4aa01bbce65279adf7d232d8246b235eb69
Jakub Urban <urban@ipp.cas.cz>
2016-05-23 21:13:38 +0200

    pyhdc first prototype

---
+++ src/pyhdc.py
@@ -0,0 +1,150 @@
+import ctypes
+from ctypes import byref
+import numpy as np
+import six
+
+__all__ = ['HDC']
+
+
+HDC_TYPES = {
+    2: ctypes.c_int8
+}
+
+
+class _HDC_T(ctypes.Structure):
+    """HDC C pointer (opaque)
+    """
+    pass
+
+_HDC_T_P = ctypes.POINTER(_HDC_T)
+
+
+libchdc = ctypes.cdll.LoadLibrary('libchdc.so')
+_hdc_new_empty = libchdc.hdc_new_empty
+_hdc_new_empty.restype = _HDC_T_P
+_hdc_as_int_1d = libchdc.hdc_as_int_1d
+_hdc_as_int_1d.restype = ctypes.POINTER(ctypes.c_long)
+_hdc_as_voidptr = libchdc.hdc_as_voidptr
+_hdc_as_voidptr.restype = ctypes.c_void_p
+_hdc_get_child = libchdc.hdc_get_child
+_hdc_get_child.restype = ctypes.POINTER(ctypes.c_int8)
+_hdc_has_child = libchdc.hdc_has_child
+_hdc_has_child.restype = ctypes.c_bool
+_hdc_get_type = libchdc.hdc_get_type
+_hdc_get_type.restype = ctypes.c_uint8
+_hdc_get_slice = libchdc.hdc_get_slice
+_hdc_get_slice.restype = _HDC_T_P
+_hdc_get_shape = libchdc.hdc_get_shape
+_hdc_get_shape.restype = ctypes.POINTER(ctypes.c_long)
+_hdc_get_ndim = libchdc.hdc_get_ndim
+_hdc_get_ndim.restype = ctypes.c_int8
+
+
+class HDC(object):
+    """HDC Python binding"""
+    def __init__(self, data=None):
+        super(HDC, self).__init__()
+        self._c_ptr = _hdc_new_empty()
+        if data is not None:
+            self.set_data(data)
+
+    @classmethod
+    def from_c_ptr(cls, c_ptr):
+        self = object.__new__(cls)
+        self._c_ptr = c_ptr
+        return self
+
+    @property
+    def c_ptr(self):
+        return self._c_ptr
+
+    @c_ptr.setter
+    def c_ptr(self, value):
+        raise ValueError("c_ptr cannot be set")
+
+    @c_ptr.deleter
+    def c_ptr(self, value):
+        raise ValueError("c_ptr cannot be deleted")
+
+    def __setitem__(self, key, value):
+        if isinstance(key, six.string_types):
+            ckey = key.encode()
+            if not _hdc_has_child(self._c_ptr, ckey):
+                new_hdc = self.__class__(value)
+                # ckey = ctypes.c_char_p(key.encode())
+                libchdc.hdc_add_child(self._c_ptr, ckey, new_hdc._c_ptr)
+            else:
+                self[key].set_data(value)
+
+        else:
+            return _hdc_get_slice(self._c_ptr, key)
+
+    def __getitem__(self, key):
+        if isinstance(key, six.string_types):
+            ckey = key.encode()
+            if not _hdc_has_child(self._c_ptr, ckey):
+                raise KeyError('key not found')
+            res = self.from_c_ptr(_hdc_get_child(self._c_ptr, ckey))
+            return res
+        else:
+            ckey = ctypes.c_size_t(key)
+            res = self.from_c_ptr(_hdc_get_slice(self._c_ptr, ckey))
+            return res
+
+    def set_data(self, data):
+        """Store data into the container
+        """
+        self.pydata = data
+        if isinstance(data, np.ndarray):
+            # cdata = np.ctypeslib.as_ctypes(data)
+            cshape = np.ctypeslib.as_ctypes(np.array(data.shape, dtype=np.int64))
+            cndim = ctypes.c_int8(data.ndim)
+            if np.issubdtype(data.dtype, np.int8):
+                cdata = data.ctypes.data_as(ctypes.POINTER(ctypes.c_int8))
+                libchdc.hdc_set_data_int8(self._c_ptr, cndim, byref(cshape), cdata)
+            elif np.issubdtype(data.dtype, np.int64):
+                cdata = data.ctypes.data_as(ctypes.POINTER(ctypes.c_int64))
+                libchdc.hdc_set_data_int64(self._c_ptr, cndim, byref(cshape), cdata)
+            elif np.issubdtype(data.dtype, np.float_):
+                cdata = data.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
+                libchdc.hdc_set_data_double(self._c_ptr, cndim, byref(cshape), cdata)
+
+    def get_ctype(self):
+        # ctype = _hdc_get_type(self._c_ptr)
+        # ctype = ctypes.c_int8
+        ctype = ctypes.c_double
+        return ctype
+
+    def get_shape(self):
+        ndim = _hdc_get_ndim(self._c_ptr)
+        cshape = _hdc_get_shape(self._c_ptr)
+        shape = tuple((cshape[i] for i in range(ndim)))
+        return shape
+
+    def as_array(self):
+        ctype = self.get_ctype()
+        if ctype == ctypes.c_int8:
+            cdata = _hdc_as_int_1d(self._c_ptr)
+            res = np.ctypeslib.as_array(cdata, self.get_shape())
+        if ctype == ctypes.c_double:
+            cdata = _hdc_as_voidptr(self._c_ptr)
+            # cdata = ctypes.POINTER(ctypes.c_double)(cdata)
+            cdata = ctypes.cast(cdata, ctypes.POINTER(ctypes.c_double))
+            res = np.ctypeslib.as_array(cdata, self.get_shape())
+
+        return res
+
+
+if __name__ == '__main__':
+
+    tree = HDC()
+
+    # pydata = np.arange(1, 8, 2, dtype=np.int8)
+    pydata = np.arange(1, 8, 2, dtype=np.float_)
+    tree["group1/int8_data"] = pydata
+
+    fmodule = ctypes.cdll.LoadLibrary('libhdc_fortran_module.so')
+
+    # change_data = fmodule.change_data
+    # change_data.restype = ctypes.c_void_p
+    # change_data.argtypes = (hdc.hdc_t, )
