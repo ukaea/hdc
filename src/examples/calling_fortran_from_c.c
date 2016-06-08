8e105430e6406914c00e3b03a166334dee932dce
David Fridrich <d.fridrich@volny.cz>
2016-06-08 08:00:53 +0200

    Added Example of calling Fortran from C

---
+++ src/examples/calling_fortran_from_c.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "types.h"
+#include <string.h>
+//#include "hdc_c.h"
+
+void* __hdc_fortran_MOD_hdc_new_ptr();
+void* __hdc_fortran_MOD_hdc_get_ptr_f(struct hdc_t* tree);
+void __hdc_fortran_MOD_hdc_add_child(struct hdc_t* tree,char* path,struct hdc_t* node);
+
+
+struct hdc_t* hdc_new_empty();
+int main(int argc, char **argv) {
+    struct hdc_t* tree = (struct hdc_t*)__hdc_fortran_MOD_hdc_new_ptr();
+    struct hdc_t* node = (struct hdc_t*)__hdc_fortran_MOD_hdc_new_ptr();
+    __hdc_fortran_MOD_hdc_add_child(&tree,"aaa/bbb/ccc",&node);
+    struct hdc_t* tree2 = (struct hdc_t*)__hdc_fortran_MOD_hdc_get_ptr_f(tree);
+    hdc_dump(tree2);
+    return 0;
+}
