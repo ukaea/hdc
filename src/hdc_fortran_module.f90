c25b56aed6896e7e9c7718224628a5e966222346
Jakub Urban <coobas@gmail.com>
2016-06-09 09:03:29 +0200

    cleaned up fortran module example

--- src/hdc_fortran_module.f90
+++ src/hdc_fortran_module.f90
@@ -2,97 +2,6 @@
 ! module hdc_fortran_module
 ! contains
 
-subroutine hello_f_module() bind(c, name="hello_f_module")
-    use hdc_fortran
-    use iso_c_binding
-    implicit none
-    print *, 'hello_f_module'
-end subroutine
-
-
-subroutine change_data(tree) bind(c, name="change_data")
-use hdc_fortran
-use iso_c_binding
-implicit none
-
-    type(hdc_t), value :: tree
-    type(hdc_t) :: node_int, node_double_2d
-    integer(kind=c_int8_t), dimension(1:4) :: array
-    ! real(kind=dp), dimension(1:4) :: array_of_double
-    integer :: i
-
-    ! print *, "Input i: "
-    ! read(*,*)i
-
-    print *, "Fortran: hdc_get_child(tree,'group1/int8_data')"
-    ! print *, tree%obj
-    print *, hdc_has_child(tree, "group1/int8_data")
-    node_int = hdc_get_child(tree, "group1/int8_data")
-
-    print *, "Reading data in Fortran"
-    array = hdc_as_int8_1d(node_int)
-    ! test data get
-    print *,"Data: ", array
-
-    array = array + 1
-    print *, "Fortran: modified data: ", array
-    print *, "Fortran: call hdc_set_data "
-    call hdc_set_data(node_int, array)
-
-    ! print *, "Fortran: get_child again"
-    ! node_int = hdc_get_child(tree, "group1/int8_data")
-
-    print *, "Reading data in Fortran"
-    array = hdc_as_int8_1d(node_int)
-    ! test data get
-    print *,"Data: ", array
-
-    print *, "Fortran: create group1/double_2d_data"
-    node_double_2d = hdc_new_empty()
-    call hdc_set_data(node_double_2d, reshape((/1.0d0,2.0d0,3.14d0,5.9d0/),(/2,2/)))
-    call hdc_add_child(tree, "group1/double_2d_data", node_double_2d)
-    print *,"Fortran: hdc_as_double_2d: ", (hdc_as_double_2d(node_double_2d))
-
-end subroutine change_data
-
-
-subroutine c_test_cpos(equilibriumin) bind(c, name="c_test_cpos")
-    use hdc_fortran
-    use iso_c_binding
-    implicit none
-    ! type(hdc_t), value :: equilibriumin
-    type(hdc_t), value :: equilibriumin
-    ! type(hdc_t_p) :: distsourceout
-    type(hdc_t) :: distsourceout
-    real(kind=DP), dimension(:), pointer :: psi
-    real(8) :: time
-
-    write(*,*)'=== c_test_cpos ==='
-    call hdc_dump(equilibriumin)
-
-    write(0,*) 'size of input CPO = ', hdc_get_shape(equilibriumin)
-    write(0,*) 'Received input time from equilibrium: ', hdc_as_double(equilibriumin, 'time')
-
-    time = hdc_as_double(equilibriumin, 'time')
-    ! call hdc_get(equilibriumin, 'time', time)
-    write(0, *)'timein', time
-
-    ! call hdc_get(equilibriumin, 'profiles_1d_psi', psi)
-    ! write(*,*)'psi = ', psi
-
-    write(*,*)'get equilibriumin/profiles_1d_psi'
-    call hdc_get(equilibriumin, 'profiles_1d_psi', psi)
-    ! psi => hdc_as_double_1d(equilibriumin, 'profiles_1d_psi')
-    write(*,*)psi
-
-    call test_cpos(equilibriumin, distsourceout)
-
-    write(*,*)'distsourceout in c_test_cpos'
-    call hdc_dump(distsourceout)
-
-    write(*,*)'=== END c_test_cpos ==='
-end subroutine
-
 
 subroutine test_cpos_f2c(equilibriumin, tree_out) bind(c, name="test_cpos_f2c")
     use hdc_fortran
@@ -202,30 +111,30 @@ subroutine test_cpos(equilibriumin, distsourceout)
 
         !HDC In HDC, we can skip the allocate statements, it will populate the tree as necessary
         !HDC This is a bit magic as we have to implemet * operator for hdc_t -- Do we want to do this?
-! !             call hdc_set(distsource_i, 'source/profiles_1d_psi', 2 * hdc_get_child(equilibrium_i, 'profiles_1d_psi'))
+! !             call hdc_set(distsource_i, 'source/profiles_1d/psi', 2 * hdc_get_child(equilibrium_i, 'profiles_1d/psi'))
         !HDC The alternative, more explicit solutions are
         !HDC it seems that hdc_get_<type> functions will be handy in Fortran
 ! read *
-! print *,hdc_as_double_1d(equilibrium_i, 'profiles_1d_psi')
+! print *,hdc_as_double_1d(equilibrium_i, 'profiles_1d/psi')
 ! read *
 
-        write(*,*)'set distsource_i/source/profiles_1d_psi'
+        write(*,*)'set distsource_i/source/profiles_1d/psi'
         ! or
-        write(*,*)'get equilibrium_i/profiles_1d_psi'
-        call hdc_get(equilibrium_i, 'profiles_1d_psi', psi)
-        ! psi = hdc_as_double(equilibrium_i, 'profiles_1d_psi')
+        write(*,*)'get equilibrium_i/profiles_1d/psi'
+        call hdc_get(equilibrium_i, 'profiles_1d/psi', psi)
+        ! psi = hdc_as_double(equilibrium_i, 'profiles_1d/psi')
         write(*,*)'psi=', psi
-        write(*,*)'set distsource_i/source/profiles_1d_psi'
+        write(*,*)'set distsource_i/source/profiles_1d/psi'
 
-        ! call hdc_set(distsource_i, 'source/profiles_1d_psi', 2 * hdc_as_double_1d(equilibrium_i, 'profiles_1d_psi'))
+        ! call hdc_set(distsource_i, 'source/profiles_1d/psi', 2 * hdc_as_double_1d(equilibrium_i, 'profiles_1d/psi'))
         psi_test = (/-1.d0, -2.d0, 3.d0, 4.4d0/)
-        ! call hdc_set(distsource_i, 'source/profiles_1d_psi', -3 * psi(:))  ! doesn work
-        ! call hdc_set(distsource_i, 'source/profiles_1d_psi', psi_test)  ! this works only in current scope (subroutine / function)
-        call hdc_set(distsource_i, 'source/profiles_1d_psi', psi)  ! this works as expected
+        ! call hdc_set(distsource_i, 'source/profiles_1d/psi', -3 * psi(:))  ! doesn work
+        ! call hdc_set(distsource_i, 'source/profiles_1d/psi', psi_test)  ! this works only in current scope (subroutine / function)
+        call hdc_set(distsource_i, 'source/profiles_1d/psi', psi)  ! this works as expected
         write(*,*) 2*psi
 
-        call hdc_get(distsource_i, 'source/profiles_1d_psi', psi)
-        ! psi = hdc_as_double(equilibrium_i, 'profiles_1d_psi')
+        call hdc_get(distsource_i, 'source/profiles_1d/psi', psi)
+        ! psi = hdc_as_double(equilibrium_i, 'profiles_1d/psi')
         ! write(*,*)'psi (distsource_i) =', psi
 
 
@@ -257,24 +166,4 @@ subroutine test_cpos(equilibriumin, distsourceout)
 end subroutine test_cpos
 
 
-subroutine test_test_cpo_hdc()
-    use hdc_fortran
-    use iso_c_binding
-    implicit none
-
-    real(8), pointer, dimension(:) :: time
-
-    type(hdc_t) :: equilibrium, distsource
-
-    equilibrium = hdc_new_empty()
-    call hdc_set(equilibrium, 'time', 1.1d0)
-    write(*,*)'hdc_get'
-    ! call hdc_get(equilibrium, 'time', time)
-    write(*,*)hdc_as_double(equilibrium, 'time')
-    call hdc_set(equilibrium, 'profiles_1d_psi', (/0.1d0, 0.2d0, 0.3d0/))
-    write(*,*)'call test_cpos'
-    call test_cpos(equilibrium, distsource)
-end subroutine
-
-    
 ! end module
--- src/hdc_fortran_module.py
+++ src/hdc_fortran_module.py
@@ -6,79 +6,17 @@ import numpy as np
 fmodule = ctypes.cdll.LoadLibrary('libhdc_fortran_module.so')
 
 
-def change_data(data):
-    func = fmodule.change_data
-    func.restype = ctypes.c_void_p
-    func.argtypes = (_HDC_T_P, )
-
-    func(data)
-
-    return data
-
-
-def test_cpos(equilibrium):
-    distsource = HDC()
-    func = fmodule.test_cpos
-    func.restype = ctypes.c_void_p
-    # func.argtypes = (_HDC_T_P, )
-    func(equilibrium.c_ptr, distsource.c_ptr)
-
-    return distsource
-
-
-def test_test_cpos():
-    import numpy as np
-
-    # for dtype in (np.float_, np.int32, ):
-    #     tree = HDC()
-    #     pydata = np.arange(1, 8, 2, dtype=dtype) * 0.1
-    #     tree["data"] = pydata
-    #     data = tree["data"].as_array()
-    #     print(data)
-    #     assert np.all(pydata == data)
-    #     assert pydata.dtype == data.dtype
-
-    # equilibrium = HDC()
-    for i in range(2):
-        print(i)
-        # equi_slice = HDC()
-        # equi_slice['time'] = float(i)
-        # equi_slice['profiles_1d'] = HDC()
-        for j in range(2):
-            print("j = %d" % j)
-            profiles_1d = HDC()
-            # put some data
-            pydata = np.arange(4, dtype=np.float) * 0.1 + i * 10 + j
-            # print('nrefs: {}'.format(sys.getrefcount(pydata)))
-            # print(id(pydata))
-            profiles_1d['psi'] = pydata
-            print('-- nrefs after: {}'.format(sys.getrefcount(pydata)))
-            # print(profiles_1d['psi']._np_data is pydata)
-            # buf = np.getbuffer(pydata)
-            # buf = memoryview(pydata.data)
-            # print(buf)
-            del pydata
-            # del buf
-            # profiles_1d['psi'] = np.arange(4, dtype=np.float) * 0.1 + i * 10 + j
-            # profiles_1d['psi'] = np.random.rand(4)
-            # append slice
-            # equi_slice['profiles_1d'].append(profiles_1d)
-            # or using set_slice
-            # equi_slice['profiles_1d'][j] = profiles_1d
-            psi = profiles_1d['psi'].as_array()
-            print(psi)
-            print(psi.dtype)
-            # assert np.all(pydata == psi)
-
-            # equilibrium.append(equi_slice)
-
-            # return equilibrium
-
-
 def test_cpos_f2c(equilibriumin):
+    """test_cpos_f2c Python wrapper
 
+    Args:
+        equilibriumin(HDC): input
+        result(HDC): output container
+    """
+
+    # in the Python / C wrapper, the output tree must be constructed
     tree = HDC()
-
+    # the Fortran wrapper is called with output tree already allocated
     fmodule.test_cpos_f2c(equilibriumin.c_ptr, tree.c_ptr)
 
     return tree
@@ -88,15 +26,15 @@ if __name__ == '__main__':
     # equilibrium = test_test_cpos()
 
     equilibrium = HDC()
-    equilibrium['profiles_1d_psi'] = np.linspace(0, 1, 5)
-    # equilibrium['profiles_1d_psi'] = np.array(4.5)
+    equilibrium['profiles_1d/psi'] = np.linspace(0, 1, 5)
+    # equilibrium['profiles_1d/psi'] = np.array(4.5)
     # libchdc.hdc_set_data_double_path(
     #     equilibrium.c_ptr, "time".encode(), ctypes.c_int8(0),
     #     ctypes.byref(ctypes.c_int64(0)), ctypes.byref(ctypes.c_double(1.1)))
     equilibrium['time'] = np.array(2.34)
 
     print("equilibrium['time'] -> {}".format(equilibrium['time'].as_array()))
-    print("equilibrium['profiles_1d_psi'] -> {}".format(equilibrium['profiles_1d_psi'].as_array()))
+    print("equilibrium['profiles_1d/psi'] -> {}".format(equilibrium['profiles_1d/psi'].as_array()))
 
     # print('py call c_test_cpos')
     # fmodule.c_test_cpos(equilibrium.c_ptr)
@@ -107,7 +45,7 @@ if __name__ == '__main__':
     print("=== Python dump")
     tree.dump()
 
-    print('tree["distsourceout/source/profiles_1d_psi"] %s' %
-          tree["distsourceout/source/profiles_1d_psi"].as_array())
+    print('tree["distsourceout/source/profiles_1d/psi"] %s' %
+          tree["distsourceout/source/profiles_1d/psi"].as_array())
 
     # distsource = test_cpos(equilibrium)
