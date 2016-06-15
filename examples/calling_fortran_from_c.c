2bf8cb6412d10435e917bad27b015075213a6944
David Fridrich <d.fridrich@volny.cz>
2016-06-15 08:41:08 +0200

    Calling Fortran to C remover from build.

--- examples/calling_fortran_from_c.c
+++ examples/calling_fortran_from_c.c
@@ -1,7 +1,17 @@
+/**
+ * This is not intended to be run directly, there are differences C and fortran string making this complicated. I have just tested the following code aftet wrapping the path string inside fortran hdc_add_child function using c_to_f_string. The better option would be to do that in C, but I don't see any reason for further investications of this. The gfortran prefixes are the same story.
+ */
+
 #include <stdio.h>
 #include "types.h"
 #include <string.h>
 //#include "hdc_c.h"
+
+char* c_str2fortran(char* str) {
+    char* f_str = (char*)malloc(strlen(str)*sizeof(char));
+    memcpy(f_str,str,strlen(str)*sizeof(char));
+    return f_str;
+}
 
 void* __hdc_fortran_MOD_hdc_new_ptr();
 void* __hdc_fortran_MOD_hdc_get_ptr_f(struct hdc_t* tree);
