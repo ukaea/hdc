067cf3bc9422d16d030109b8b2b1b5cc0c9a659d
David Fridrich <d.fridrich@volny.cz>
2016-06-28 09:26:18 +0200

    Nothing.

---
+++ test/test_hdc_fortran.f90
@@ -0,0 +1,8 @@
+! Nothing here for now, just some pieces of code...
+
+
+! ! Try to add empty array of doubles
+! data = hdc_new_dtype(int(1,1),int((/4/),8),"double")
+! print *,"type: ",hdc_get_type(data)
+! print *,"ndim: ",hdc_get_ndim(data)
+! print *,"shape: ",hdc_get_shape(data)
