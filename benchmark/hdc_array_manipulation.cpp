972cf0a5f72d6e4036ec6c274af4c7eee05b55dd
David Fridrich <d.fridrich@volny.cz>
2016-08-22 14:55:57 +0200

    Benchmarks are back (but still segfault).

--- benchmark/hdc_array_manipulation.cpp
+++ benchmark/hdc_array_manipulation.cpp
@@ -1,18 +1,17 @@
 #include "hdc.hpp"
 #include "benchmark/benchmark.h"
 #include <string>
-
 using namespace std;
-/*
+
 // Plain memcpy test to show over overhead of HDC
 static void BM_memcpy(benchmark::State& state) {
-    size_t n = state.range_x();
+    size_t n = state.range(0);
     int32_t* src = new int32_t[n];
     int32_t* dst = new int32_t[n];
     memset(src, 1, n);
     while (state.KeepRunning())
         memcpy(dst, src, n*sizeof(int32_t));
-    state.SetBytesProcessed(int64_t(state.iterations()) * int64_t(state.range_x()) * int64_t(sizeof(int32_t)));
+    state.SetBytesProcessed(int64_t(state.iterations()) * int64_t(state.range(0)) * int64_t(sizeof(int32_t)));
     delete[] src;
     delete[] dst;
 }
@@ -21,14 +20,14 @@ BENCHMARK(BM_memcpy)->RangeMultiplier(2)->Range(1024*1024,1024*1024<<5);
 // Set Data
 static void BM_HDC_SetData(benchmark::State& state) {
     HDC* node = new HDC();
-    long shape[1];
-    shape[0] = state.range_x();
-    int32_t* data = new int32_t[state.range_x()];
-    memset(data,1,sizeof(int32_t)*state.range_x());
+    size_t shape[1];
+    shape[0] = state.range(0);
+    int32_t* data = new int32_t[state.range(0)];
+    memset(data,1,sizeof(int32_t)*state.range(0));
     while (state.KeepRunning()) {
-        node->set_data<int32_t>(1,shape,(void*)data);
+        node->set_data(1,shape,data);
     }
-    state.SetBytesProcessed(int64_t(state.iterations()) * int64_t(state.range_x()) * int64_t(sizeof(int32_t)));
+    state.SetBytesProcessed(int64_t(state.iterations()) * int64_t(state.range(0)) * int64_t(sizeof(int32_t)));
     delete node;
     delete[] data;
 }
@@ -38,14 +37,14 @@ BENCHMARK(BM_HDC_SetData)->RangeMultiplier(2)->Range(1024*1024,1024*1024<<5);
 // Set Data - Zero Copy
 static void BM_HDC_SetDataZeroCopy(benchmark::State& state) {
     HDC* node = new HDC();
-    long shape[1];
-    shape[0] = state.range_x();
-    int32_t* data = new int32_t[state.range_x()];
-    memset(data,1,sizeof(int32_t)*state.range_x());
+    size_t shape[1];
+    shape[0] = state.range(0);
+    int32_t* data = new int32_t[state.range(0)];
+    memset(data,1,sizeof(int32_t)*state.range(0));
     while (state.KeepRunning()) {
-        node->set_data<int32_t>(1,shape,(void*)data,false);
+        node->set_data(1,shape,data,false);
     }
-//    state.SetBytesProcessed(int64_t(state.iterations()) * int64_t(state.range_x()) * int64_t(sizeof(int32_t)));
+//    state.SetBytesProcessed(int64_t(state.iterations()) * int64_t(state.range(0)) * int64_t(sizeof(int32_t)));
     delete node;
     delete[] data;
 }
@@ -54,11 +53,11 @@ BENCHMARK(BM_HDC_SetDataZeroCopy)->RangeMultiplier(2)->Range(1024,1024<<5);
 // As Data
 static void BM_HDC_GetData(benchmark::State& state) {
     HDC* node = new HDC();
-    long shape[1];
-    shape[0] = state.range_x();
-    int32_t* data = new int32_t[state.range_x()];
-    memset(data,1,sizeof(int32_t)*state.range_x());
-    node->set_data<int32_t>(1,shape,(void*)data);
+    size_t shape[1];
+    shape[0] = state.range(0);
+    int32_t* data = new int32_t[state.range(0)];
+    memset(data,1,sizeof(int32_t)*state.range(0));
+    node->set_data(1,shape,data);
     while (state.KeepRunning()) {
         int32_t* data2 = node->as<int32_t*>();
     }
@@ -66,4 +65,3 @@ static void BM_HDC_GetData(benchmark::State& state) {
     delete[] data;
 }
 BENCHMARK(BM_HDC_GetData)->RangeMultiplier(2)->Range(1024,1024<<5);
-*/
--- benchmark/hdc_node_manipulation.cpp
+++ benchmark/hdc_node_manipulation.cpp
@@ -1,7 +1,7 @@
 #include "hdc.hpp"
 #include "benchmark/benchmark.h"
 #include <string>
-/*
+
 using namespace std;
 // Empty child creation and deletion
 static void BM_HDC_CreationAndDeletion(benchmark::State& state) {
@@ -17,17 +17,17 @@ BENCHMARK(BM_HDC_CreationAndDeletion);
 static void BM_HDC_AddChild(benchmark::State& state) {
     while (state.KeepRunning()) {
         HDC* tree = new HDC();
-        for (size_t i = 0;i < state.range_x();i++) tree->add_child(std::to_string(i),new HDC());
+        for (size_t i = 0;i < state.range(0);i++) tree->add_child(std::to_string(i),new HDC());
         delete tree;
     }
-    state.SetItemsProcessed(state.range_x() * state.iterations());
+    state.SetItemsProcessed(state.range(0) * state.iterations());
 }
 BENCHMARK(BM_HDC_AddChild)->RangeMultiplier(2)->Range(1024,1024<<5);
 
 // Adding child to deep path
 static void BM_HDC_AddChildPathDepth(benchmark::State& state) {
     string path = "";
-    for (size_t k=0;k<state.range_x();k++) path += "aaa/";
+    for (size_t k=0;k<state.range(0);k++) path += "aaa/";
     while (state.KeepRunning()) {
         HDC* tree = new HDC();
         tree->add_child(path,new HDC());
@@ -40,7 +40,7 @@ BENCHMARK(BM_HDC_AddChildPathDepth)->RangeMultiplier(2)->Range(1,16);
 // Geting child from deep path
 static void BM_HDC_GetChildPathDepth(benchmark::State& state) {
     string path = "";
-    for (size_t k=0;k<state.range_x();k++) path += "aaa/";
+    for (size_t k=0;k<state.range(0);k++) path += "aaa/";
     HDC* tree = new HDC();
     tree->add_child(path,new HDC());
     while (state.KeepRunning()) {
@@ -55,7 +55,7 @@ BENCHMARK(BM_HDC_GetChildPathDepth)->RangeMultiplier(2)->Range(1,16);
 // Adding child to path with long string
 static void BM_HDC_AddChildPathLength(benchmark::State& state) {
     string path = "";
-    for (size_t k=0;k<state.range_x();k++) path += "a";
+    for (size_t k=0;k<state.range(0);k++) path += "a";
     while (state.KeepRunning()) {
         HDC* tree = new HDC();
         tree->add_child(path,new HDC());
@@ -68,7 +68,7 @@ BENCHMARK(BM_HDC_AddChildPathLength)->RangeMultiplier(2)->Range(1024,1024<<5);
 // Getting child to path with long string
 static void BM_HDC_GetChildPathLength(benchmark::State& state) {
     string path = "";
-    for (size_t k=0;k<state.range_x();k++) path += "a";
+    for (size_t k=0;k<state.range(0);k++) path += "a";
     HDC* tree = new HDC();
     tree->add_child(path,new HDC());
     while (state.KeepRunning()) {
@@ -82,19 +82,19 @@ BENCHMARK(BM_HDC_GetChildPathLength)->RangeMultiplier(2)->Range(1024,1024<<5);
 
 // Appending slice to long (but not deep) path
 static void BM_HDC_AppendSlice(benchmark::State& state) {
-    state.SetItemsProcessed(state.range_x());
+    state.SetItemsProcessed(state.range(0));
     while (state.KeepRunning()) {
         HDC* tree = new HDC();
-        for (size_t i=0;i<state.range_x();i++) tree->append_slice(new HDC());
+        for (size_t i=0;i<state.range(0);i++) tree->append_slice(new HDC());
         delete tree;
     }
-    state.SetItemsProcessed(state.range_x() * state.iterations());
+    state.SetItemsProcessed(state.range(0) * state.iterations());
 }
 BENCHMARK(BM_HDC_AppendSlice)->RangeMultiplier(2)->Range(1024,1024<<5);
 
 // Test has_child()
 static void BM_HDC_HasChildMultipleItems(benchmark::State& state) {
-    int n = state.range_x();
+    int n = state.range(0);
     HDC* tree = new HDC();
     for(int i=0;i<n;i++) tree->add_child(to_string(i),new HDC());
     while (state.KeepRunning()) {
@@ -110,7 +110,7 @@ BENCHMARK(BM_HDC_HasChildMultipleItems)->RangeMultiplier(2)->Range(1024,1024<<5)
 
 // Test get()
 static void BM_HDC_GetChildMultipleItems(benchmark::State& state) {
-    int n = state.range_x();
+    int n = state.range(0);
     HDC* tree = new HDC();
     for(int i=0;i<n;i++) tree->add_child(to_string(i),new HDC());
     while (state.KeepRunning()) {
@@ -126,7 +126,7 @@ BENCHMARK(BM_HDC_GetChildMultipleItems)->RangeMultiplier(2)->Range(1024,1024<<5)
 
 // Test get_slice()
 static void BM_HDC_GetSliceMultipleItems(benchmark::State& state) {
-    int n = state.range_x();
+    int n = state.range(0);
     HDC* tree = new HDC();
     for(int i=0;i<n;i++) tree->append_slice(new HDC());
     while (state.KeepRunning()) {
@@ -139,4 +139,3 @@ static void BM_HDC_GetSliceMultipleItems(benchmark::State& state) {
     delete tree;
 }
 BENCHMARK(BM_HDC_GetSliceMultipleItems)->RangeMultiplier(2)->Range(1024,1024<<5);
-*/
