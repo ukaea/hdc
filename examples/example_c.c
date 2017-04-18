11b48de7c946f14418ab8310d43a2faec9d4efe2
Jakub Urban <coobas@gmail.com>
2017-04-18 10:36:48 +0200

    C example with a function call
    Demonstrates a call to manipulate(struct hdc_t* data)

--- examples/example_c.c
+++ examples/example_c.c
@@ -8,9 +8,9 @@ int main() {
     struct hdc_t* tree = hdc_new_empty();
     
     // Add some children
-    hdc_add_child(tree,"aaa/bbb/ccc",hdc_new_empty());
-    hdc_add_child(tree,"aaa/bbb/eee",hdc_new_empty());
-    hdc_add_child(tree,"bbb/eee/aaa",hdc_new_empty());
+    hdc_add_child(tree,"groupA/data/int_array",hdc_new_empty());
+    hdc_add_child(tree,"groupA/data/junk",hdc_new_empty());
+    hdc_add_child(tree,"GroupB/data/empty",hdc_new_empty());
     // Test hdc_keys() here:
     /*
     size_t n;
@@ -19,15 +19,18 @@ int main() {
     printf("\n");
     */
     // Get subtree
-    struct hdc_t* subtree = hdc_get(tree,"aaa/bbb");
+    struct hdc_t* subtree = hdc_get(tree,"groupA/data");
     // Get node
-    struct hdc_t* node = hdc_get(subtree,"ccc");
+    struct hdc_t* node = hdc_get(subtree,"int_array");
     
     // Ask whether child exists
-    printf("has_child: %d\n", hdc_has_child(tree,"aaa/bbb/ccc"));
+    printf("has_child: %d (should be true)\n", hdc_has_child(tree, "groupA/data/int_array"));
+    printf("has_child: %d (should be false)\n", hdc_has_child(tree, "groupA/data/this_does_not_exist"));
     
+    // Delete node
+    hdc_delete_child(tree,"GroupA/data/junk");
     // Delete subtree
-    hdc_delete_child(tree,"aaa/eee");
+    hdc_delete_child(tree,"GroupB/data");
     
     // Prepare some data
     int32_t array[4] = {7,2,3,4};
@@ -39,7 +42,7 @@ int main() {
     hdc_set_int32(data,1,shape,(void*)array);
     
     // Add data to a subtree
-    hdc_set_int32_path(tree,"aaa/bbb/ccc",1,shape,(void*)array);
+    hdc_set_int32_path(tree,"groupA/data/int_array",1,shape,(void*)array);
     
     // Ask on some data details, use subtree to shorten the path
     int32_t ndim2 = hdc_get_ndim(node);
@@ -54,10 +57,35 @@ int main() {
     printf("Data: ");
     for (int i=0; i<shape2[0]; i++) printf("%d ",array2[i]);
     printf("\n");
+
+    // Modify the data
+    manipulate(tree);
     
     //Serialize data to JSON
     hdc_to_json(tree,"tree.txt",0);
     // Dump to screen
+    printf("Final dump:\n");
     hdc_dump(tree);
     return 0;
+}
+
+void manipulate(struct hdc_t* data) {
+    printf("-------------------\n");
+    printf("Entering manipulate\n");
+    printf("Input data:\n");
+    hdc_dump(data);
+    // Get the HDC node
+    struct hdc_t* node = hdc_get(data, "groupA/data/int_array");
+    // Get the pointer to the data
+    int32_t* array2 = hdc_as_int32_1d(node);
+    // and shape
+    int32_t ndim2 = hdc_get_ndim(node);
+    long int* shape2 = hdc_get_shape(node);
+    // Multiply by 2
+    for (int i=0; i<shape2[0]; i++) 
+        array2[i] *= 2;
+    printf("Output data:\n");
+    hdc_dump(data);
+    printf("-------------------\n");
+
 }
