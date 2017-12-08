68daf413014988206bdfd332fe065555a8bccf4a
Jakub Urban <coobas@gmail.com>
2017-12-08 11:18:05 +0100

    Test creating new HDC inside cython - ok

--- src/cyhdc.pyx
+++ src/cyhdc.pyx
@@ -1,3 +1,4 @@
+# -*- coding: utf-8
 # https://github.com/cython/cython/wiki/WrappingCPlusPlus
 # https://dmtn-013.lsst.io/
 
@@ -12,14 +13,22 @@ cdef extern from "hdc.hpp":
     cdef cppclass HDC:
         HDC()
         string serialize()
-        string to_json_string(int mode)
+        string to_json_string(int mode = 0)
+        void set_child(string path, HDC* n)
+        void add_child(string path, HDC* n)
+        void set_string(string data)
 
 
 cdef class pyHDC:
-    cdef unique_ptr[HDC] thisptr
+    # cdef unique_ptr[HDC] thisptr
+    cdef HDC* thisptr
 
     def __init__(self):
-        self.thisptr.reset(new HDC())
+        self.thisptr = new HDC()
+        # create a new HDC object to test that it's not deallocated
+        cdef HDC* n = new HDC()
+        deref(self.thisptr).add_child('test', n)
+        deref(n).set_string('n-data Ͼ Ϣ')
 
     def dumps(self):
-        return deref(self.thisptr).serialize()
+        return deref(self.thisptr).to_json_string().decode()
