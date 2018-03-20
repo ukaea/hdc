db5334036744a5eeff91bed4d6402c85dd5016a8
David Fridrich <d.fridrich@volny.cz>
2018-03-20 17:39:25 +0100

    Removed unnecessary code (moved to separate file in the previous commit)

--- benchmark/hdc_benchmark_fortran.f90
+++ benchmark/hdc_benchmark_fortran.f90
@@ -1,16 +1,3 @@
-module c_perf
-  use iso_c_binding
-  implicit none
-
-  interface
-     subroutine c_getMillis(t) bind(c,name='getMillis')
-       use iso_c_binding
-       integer(kind=c_long_long), intent(out) :: t
-     end subroutine c_getMillis
-  end interface
-
-end module c_perf
-
 program benchmark_fortran
     use hdc_fortran
     use iso_c_binding
--- benchmark/hdc_benchmark_put_all.f90
+++ benchmark/hdc_benchmark_put_all.f90
@@ -1,17 +1,3 @@
-module c_perf
-  use iso_c_binding
-  implicit none
-
-  interface
-     subroutine c_getMillis(t) bind(c,name='getMillis')
-       use iso_c_binding
-       integer(kind=c_long_long), intent(out) :: t
-     end subroutine c_getMillis
-  end interface
-
-end module c_perf
-
-
 program hdc_benchmark_put_all
     use hdc_fortran
     use c_perf
