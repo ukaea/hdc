748d6ad5871bb63ec15fbaa2ff46b08cbbb2fe9f
Jakub Urban <coobas@gmail.com>
2018-03-28 14:16:49 +0200

    WIP: initial setup.py
    to be placed into install folder / python (with .pyx source)
    cmake must be configure
    header files should be in install

---
+++ src/setup.py
@@ -0,0 +1,41 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Build import cythonize
+import os
+
+extensions = [
+    Extension(
+        name="cyhdc",
+        sources=["cyhdc.pyx"],
+        language='c++',
+        # TODO fix paths after headers are installed
+        include_dirs=[
+            os.path.join('..', '..', 'include'),
+            os.path.join('..', '..', 'thirdparty'),
+            os.path.join('..', '..', 'thirdparty', 'jsoncpp'),
+            os.path.join('..', '..', 'thirdparty', 'Pluma-1.1', 'include'),
+        ],
+        libraries=['hdc'],
+        library_dirs=[os.path.join('..', 'lib')],
+        runtime_library_dirs=[os.path.abspath(os.path.join('..', 'lib'))], )
+]
+
+options = dict(
+    name="pyhdc",
+    py_modules=["pyhdc"],
+    ext_modules=cythonize(extensions),
+    license='MIT',
+    description='Hierarchical Dynamic Containers',
+    author='David Fridrich, Jakub Urban',
+    author_email='fridrich@ipp.cas.cz',
+    url='https://bitbucket.org/compass-tokamak/hdc',
+    install_requires=[
+        'six',
+        'numpy',
+        'pytest',
+    ],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+)
+
+setup(**options)
