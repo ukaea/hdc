3fe820c3392e538727f90b615c33f0fe94cc9b50
David Fridrich <d.fridrich@volny.cz>
2018-04-06 17:25:52 +0200

    Removed unused variable.

--- benchmark/timer.c
+++ benchmark/timer.c
@@ -2,10 +2,7 @@
 
 void getMillis(unsigned long long *t)
 {
-  int err;
   struct timeval tv;
-  //struct timezone tz;
-
-  err = gettimeofday(&tv, 0);
+  gettimeofday(&tv, 0);
   *t = tv.tv_sec*1e3 + tv.tv_usec/1e3;
 }
