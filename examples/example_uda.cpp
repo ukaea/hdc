99eeb809ceba608202287b84f849e0c3524b0ab7
David Fridrich <d.fridrich@volny.cz>
2018-05-22 11:45:38 +0200

    More signals for MDSplus

--- examples/example_uda.cpp
+++ examples/example_uda.cpp
@@ -5,6 +5,7 @@
 #include <limits.h>
 #include <vector>
 #include <string>
+#include <vector>
 using namespace std;
 
 void imasdd_plugin() {
@@ -17,10 +18,50 @@ void imasdd_plugin() {
 
 void mdsplus_plugin() {
 //     HDC h("uda://_a=1+41|MDSPLUS::localhost");
-    HDC h("uda://\\magnetics::iplasma:trapeze|MDSPLUS::localhost:8001/tcv_shot/50000");
-    //HDC h("uda://HELP::help()");
-    h.print_info();
-    h.dump();
+
+    std::vector<std::string> paths = {
+        "uda://\\magnetics::vloop[*,\"001\"]|MDSPLUS::localhost:8001/tcv_shot/60950",
+        "uda://\\magnetics::flux[*,\"001\"]|MDSPLUS::localhost:8001/tcv_shot/60950",
+        "uda://dim_of(\\magnetics::flux[*,\"001\"])|MDSPLUS::localhost:8001/tcv_shot/60950",
+        "uda://\\r_contour|MDSPLUS::localhost:8001/tcv_shot/60950",
+        "uda://\\z_contour|MDSPLUS::localhost:8001/tcv_shot/60950",
+        "uda://dim_of(\\results::r_axis)|MDSPLUS::localhost:8001/tcv_shot/60950",
+        "uda://\\magnetics::rbphi|MDSPLUS::localhost:8001/tcv_shot/60950",
+        "uda://dim_of(\\magnetics::rbphi)|MDSPLUS::localhost:8001/tcv_shot/60950",
+        "uda://\\results::fir:n_average|MDSPLUS::localhost:8001/tcv_shot/60950",
+        "uda://dim_of(\\results::fir:n_average)|MDSPLUS::localhost:8001/tcv_shot/60950",
+        "uda://\\results::proffit.avg_time:neft|MDSPLUS::localhost:8001/tcv_shot/60950",
+        "uda://dim_of(\\results::proffit.avg_time:neft)|MDSPLUS::localhost:8001/tcv_shot/60950",
+        "uda://\\results::ne_thomson:foo|MDSPLUS::localhost:8001/tcv_shot/60950",
+        "uda://\\results::ibs:z_eff|MDSPLUS::localhost:8001/tcv_shot/60950",
+        "uda://\\results::zx:comment|MDSPLUS::localhost:8001/tcv_shot/60950",
+        "uda://dim_of(\\results::zx:foo)|MDSPLUS::localhost:8001/tcv_shot/60950",
+        "uda://dim_of(\\results::toray.input:P_GYRO)|MDSPLUS::localhost:8001/tcv_shot/60950",
+        "uda://\\TCV_SHOT::TOP.RESULTS.TORAY.INPUT.RHO_TORAY|MDSPLUS::localhost:8001/tcv_shot/60950",
+        "uda://dim_of(\\TCV_SHOT::TOP.RESULTS.TORAY.INPUT.RHO_TORAY,1)|MDSPLUS::localhost:8001/tcv_shot/60950",
+        "uda://\\TCV_SHOT::TOP.RESULTS.TORAY.OUTPUT_X:PDENS|MDSPLUS::localhost:8001/tcv_shot/60950",
+        "uda://\\ATLAS::NBH.DATA.MAIN_ADC:DATA|MDSPLUS::localhost:8001/tcv_shot/60950",
+        "uda://data(\\VSYSTEM::TCV_NBHDB_I[\"NBI:LCS_MODE\"])|MDSPLUS::localhost:8001/tcv_shot/60950",
+        "uda://dim_of(\\ATLAS::NBH.DATA.MAIN_ADC:DATA,0)|MDSPLUS::localhost:8001/tcv_shot/60950",
+        "uda://dim_of(\\results::l_i)|MDSPLUS::localhost:8001/tcv_shot/60950",
+        "uda://\\results::oh_power|MDSPLUS::localhost:8001/tcv_shot/60950",
+        "uda://\\results::total_energy|MDSPLUS::localhost:8001/tcv_shot/60950",
+        "uda://\\results::beta_pol|MDSPLUS::localhost:8001/tcv_shot/60950",
+        "uda://\\results::q_psi|MDSPLUS::localhost:8001/tcv_shot/60950",
+        "uda://\\results::bolo:prad:total|MDSPLUS::localhost:8001/tcv_shot/60950",
+        "uda://dim_of(\\results::bolo:prad:total)|MDSPLUS::localhost:8001/tcv_shot/60950"
+    };
+    for (auto& path : paths) {
+        HDC h(path);
+        if (strcmp(h.get_type_str(),"null") == 0) {
+            std::cout << "EE: ";
+            std::cout << path << std::endl;
+        }
+        //h.print_info();
+    }
+
+//     h.print_info();
+//     h.dump();
     return;
 }
 
