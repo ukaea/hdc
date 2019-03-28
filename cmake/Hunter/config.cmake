eef5386c5626d2ed2e65a946a927f793d3c817f9
David Fridrich <d.fridrich@volny.cz>
2019-03-28 11:26:52 +0100

    De-hunterized back. Hunter is fine, but we can live without.

--- cmake/Hunter/config.cmake
+++
@@ -1,8 +0,0 @@
-hunter_config(Boost
-    VERSION ${HUNTER_Boost_VERSION}
-    CMAKE_ARGS BUILD_SHARED_LIBS=ON 
-)
-hunter_config(jsoncpp
-    VERSION ${HUNTER_jsoncpp_VERSION}
-    CMAKE_ARGS BUILD_SHARED_LIBS=ON
-)
