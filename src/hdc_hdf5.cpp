94a8786329b11ba3862d7497394ef756e6b4b9d5
David Fridrich <fridrich@ipp.cas.cz>
2019-09-06 13:09:33 +0200

    rStack() -> PrintError()

--- src/hdc_hdf5.cpp
+++ src/hdc_hdf5.cpp
@@ -128,35 +128,19 @@ void write_node(HDC h, H5File* file, std::string path)
     }  // end of try block
         // catch failure caused by the H5File operations
     catch (FileIException& error) {
-        #if H5_VERSION_GE(1,8,13)
-            error.printErrorStack();
-        #else
-            std::cerr << "write_node(): " << "FileIException" << std::endl;
-        #endif
+            error.printError();
     }
         // catch failure caused by the DataSet operations
     catch (DataSetIException& error) {
-        #if H5_VERSION_GE(1,8,13)
-            error.printErrorStack();
-        #else
-            std::cerr << "write_node(): " << "DataSetIException" << std::endl;
-        #endif
+            error.printError();
     }
         // catch failure caused by the DataSpace operations
     catch (DataSpaceIException& error) {
-        #if H5_VERSION_GE(1,8,13)
-            error.printErrorStack();
-        #else
-            std::cerr << "write_node(): " << "DataSpaceIException" << std::endl;
-        #endif
+            error.printError();
     }
         // catch failure caused by the DataSpace operations
     catch (DataTypeIException& error) {
-        #if H5_VERSION_GE(1,8,13)
-            error.printErrorStack();
-        #else
-            std::cerr << "write_node(): " << "DataTypeIException" << std::endl;
-        #endif
+            error.printError();
     }
     return;
 }
@@ -169,11 +153,7 @@ void HDC::to_hdf5(std::string filename, std::string dataset_name UNUSED)
         write_node(HDC(this->storage, this->uuid), &file, "data");
     }  // end of try block
     catch (FileIException& error) {
-        #if H5_VERSION_GE(1,8,13)
-            error.printErrorStack();
-        #else
-            std::cerr << "to_hdf5(): " << "FileIException" << std::endl;
-        #endif
+            error.printError();
         exit(-1);
     }
     return;
