3dadb6fc66074f3c78eec310a094bf9cd7896af0
David Fridrich <d.fridrich@volny.cz>
2020-04-07 10:51:29 +0200

    Simplified setup.py

--- examples/cython_cpp/setup.py
+++ examples/cython_cpp/setup.py
@@ -5,6 +5,10 @@ import sys
 from setuptools import setup
 from setuptools.extension import Extension
 
+# HDC root is "../.."
+hdc_root = os.path.abspath(os.path.join("..",".."))
+# Let us assume that installation prefix is ${hdc_root}/install. You can change this if necessary:
+hdc_install_prefix = os.path.abspath(os.path.join(hdc_root,"install"))
 
 extensions = [
     Extension(
@@ -13,22 +17,22 @@ extensions = [
         extra_compile_args=['-std=c++11'],
         language='c++',
         include_dirs=[
-            os.path.join("/home/david/projects/hdc", 'include'),
-            os.path.join("/home/david/projects/hdc", 'thirdparty'),
-            os.path.join("/home/david/projects/hdc", 'thirdparty', 'jsoncpp'),
-            os.path.join("/home/david/projects/hdc", 'thirdparty', 'Pluma-1.1', 'include'),
-            os.path.join("/home/david/projects/hdc", 'examples', 'cython_cpp'),
+            os.path.join(hdc_root, 'include'),
+            os.path.join(hdc_root, 'thirdparty'),
+            os.path.join(hdc_root, 'thirdparty', 'jsoncpp'),
+            os.path.join(hdc_root, 'thirdparty', 'Pluma-1.1', 'include'),
+            os.path.join(hdc_root, 'examples', 'cython_cpp'),
         ],
         libraries=["hdc",
                    "hdcdumper"
         ],
         library_dirs=[
-            os.path.abspath(os.path.join("/home/david/projects/hdc/install", 'lib')),
-            os.path.abspath(os.path.join("/home/david/projects/hdc/examples/cython_cpp"))
+            os.path.abspath(os.path.join(hdc_install_prefix, 'lib')),
+            os.path.abspath(os.path.curdir),
         ],
         runtime_library_dirs=[
-            os.path.abspath(os.path.join("/home/david/projects/hdc/install", 'lib')),
-            os.path.abspath(os.path.join("/home/david/projects/hdc/examples/cython_cpp")),
+            os.path.abspath(os.path.join(hdc_install_prefix, 'lib')),
+            os.path.abspath(os.path.curdir),
         ],
     )
 ]
