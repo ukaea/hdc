e6c08427623dbe04812d91b622d30cefeb11bd33
David Fridrich <d.fridrich@volny.cz>
2022-04-25 12:53:03 +0200

    [WIP] Decouple fortran.

--- cmake/FindLibS3.cmake
+++ cmake/FindLibS3.cmake
@@ -1,28 +1,28 @@
 # - Find LibS3
 #
 # To provide the module with a hint about where to find your LibS3
-# installation, you can set the environment variable LIBS3_ROOT. The
+# installation, you can set the environment variable LibS3_ROOT. The
 # Find module will then look in this path when searching for LibS3 paths
 # and libraries.
 #
 # Find the LibS3 includes and libraries
 #
-#  LIBS3_INCLUDE_DIR - where to find mdslib.h, etc
-#  LIBS3_LIBRARIES   - Link these libraries when using LibS3
-#  LIBS3_FOUND       - True if LibS3 found
+#  LibS3_INCLUDE_DIR - where to find mdslib.h, etc
+#  LibS3_LIBRARIES   - Link these libraries when using LibS3
+#  LibS3_FOUND       - True if LibS3 found
 #
 # Normal usage would be:
 #  find_package (LibS3 REQUIRED)
-#  target_link_libraries (uses_LibS3 ${LIBS3_LIBRARIES})
+#  target_link_libraries (uses_LibS3 ${LibS3_LIBRARIES})
 
-if( LIBS3_INCLUDE_DIR AND LIBS3_LIBRARIES )
+if( LibS3_INCLUDE_DIR AND LibS3_LIBRARIES )
   # Already in cache, be silent
-  set( LIBS3_FIND_QUIETLY TRUE )
-endif( LIBS3_INCLUDE_DIR AND LIBS3_LIBRARIES )
+  set( LibS3_FIND_QUIETLY TRUE )
+endif( LibS3_INCLUDE_DIR AND LibS3_LIBRARIES )
 
-find_path( LIBS3_INCLUDE_DIR libs3.h
-  HINTS ${LIBS3_ROOT}
-  ENV LIBS3_ROOT
+find_path( LibS3_INCLUDE_DIR LibS3.h
+  HINTS ${LibS3_ROOT}
+  ENV LibS3_ROOT
   PATHS
     /usr/local
     /opt/local
@@ -31,9 +31,9 @@ find_path( LIBS3_INCLUDE_DIR libs3.h
   PATH_SUFFIXES include
 )
 
-find_library( LIBS3_LIBRARIES NAMES s3
-  HINTS ${LIBS3_ROOT}
-  ENV LIBS3_ROOT
+find_library( LibS3_LIBRARIES NAMES s3
+  HINTS ${LibS3_ROOT}
+  ENV LibS3_ROOT
   PATHS
     /opt/local
     /sw
@@ -43,6 +43,6 @@ find_library( LIBS3_LIBRARIES NAMES s3
 )
 
 include( FindPackageHandleStandardArgs )
-find_package_handle_standard_args( LIBS3 DEFAULT_MSG LIBS3_LIBRARIES LIBS3_INCLUDE_DIR )
+find_package_handle_standard_args( LibS3 DEFAULT_MSG LibS3_LIBRARIES LibS3_INCLUDE_DIR )
 
-mark_as_advanced( LIBS3_LIBRARIES LIBS3_INCLUDE_DIR )
+mark_as_advanced( LibS3_LIBRARIES LibS3_INCLUDE_DIR )
